03028fbafde15f8ace41d3ada17101e85805e586
Leah Roberson <kireysmom12@gmail.com>
2022-11-18 14:44:48 -0600

    initial

---
+++ about.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('form submit');
+}
+
+// function (){
+
+// }
+
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
