ba0d45bfae6795eb821b6dd5928dc05c327722fb
Leah Roberson <kireysmom12@gmail.com>
2022-11-18 20:21:11 -0600

    update 2 11/18

--- about.js
+++ about.js
@@ -8,11 +8,24 @@ function handleSubmit(evt) {
 	alert('form submit');
 }
 
-// function (){
-
+// function outputImage(e){
+// 	e.mouseover();
 // }
 
+grumpyCat.addEventListener('mouseover', () =>{
+	alert("Cats are lovely and so are you.");
+})
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+var myArray = ["Joes Pizza and Pasta", "The philly shack", "Habaneros", "Magic Spring Roll", "Nepali Chulo Indian Cuisine", "Aladdin Mediterranean Café"];
+
+const button4 = document.querySelector('#restruants');
+
+const rando = Math.floor(Math.random() * myArray.length);
+
+button4.addEventListener('click', () =>{
+	alert(rando, myArray[rando]);
+});
---
+++ profile.js
@@ -0,0 +1,35 @@
+//let kuzco = "https://i.etsystatic.com/20344285/r/il/d63364/2228524776/il_fullxfull.2228524776_mfkg.jpg"
+
+// me.addEventListener('mouseover', () =>{
+// 	alert("NO TOUCHY!!!!");
+// })
+
+const button1 = document.querySelector("#color");
+
+button1.addEventListener("click", () => {
+  alert("Pink! And green... And black..");
+});
+
+const button2 = document.querySelector("#place");
+
+button2.addEventListener("click", () => {
+  alert("Destin, Florida");
+});
+
+const button3 = document.querySelector("#ritual");
+
+button3.addEventListener("click", () => {
+  alert("Listening to opera while I work???");
+});
+
+// place.addEventListener('click' () =>{
+//     alert("Destin Flordia");
+// });
+
+// ritual.addEventListener('click' () =>{
+//     alert("Listening to opera while I work???")
+// });
+
+// let button1= document.querySelector('#color');
+
+// button1.addEventListener('PINK!');
